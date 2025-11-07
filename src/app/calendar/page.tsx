11e60a2b90d1ae4511519f32a094b22a0cb8ffec
Mink Office <minkeiwu@gmail.com>
2025-11-07 14:56:49 +0800

    landscape

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,6 +1,6 @@
 const Calendar = () => {
   return (
-    <div className="flex h-[calc(100vh-6rem)] w-full p-2 text-white bg-zinc-500">
+    <div className="flex m-2 text-white bg-zinc-500 landscape:text-blue-700">
       321
     </div>
   )
